e29803d2669e2aac53c6a07d4729e9c81c6fe55d
Lucas Leonardo <llbs@cin.ufpe.br>
2022-06-04 18:02:30 -0300

    Implement a service to serve stream using gRPC

--- services/user.go
+++ services/user.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"context"
+	"time"
 
 	"github.com/lucasleonardobs/go-grpc-server/pb"
 )
@@ -22,4 +23,42 @@ func (us *UserService) AddUser(ctx context.Context, request *pb.User) (*pb.User,
 	}
 
 	return user, nil
+}
+
+func (us *UserService) AddUserVerbose(request *pb.User, stream pb.UserService_AddUserVerboseServer) error {
+	stream.Send(&pb.UserResultStream{
+		Status: "Init",
+		User:   &pb.User{},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Inserting",
+		User:   &pb.User{},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "User has been inserted",
+		User: &pb.User{
+			Id:    "123",
+			Name:  request.GetName(),
+			Email: request.GetEmail(),
+		},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Completed",
+		User: &pb.User{
+			Id:    "123",
+			Name:  request.GetName(),
+			Email: request.GetEmail(),
+		},
+	})
+
+	return nil
 }
