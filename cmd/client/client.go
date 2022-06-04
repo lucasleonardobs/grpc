87f737f6f813bb8741efa2e9c5687fb2be3c084a
Lucas Leonardo <llbs@cin.ufpe.br>
2022-06-04 17:56:58 -0300

    Update deprecated method WithInsecure

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -7,10 +7,11 @@ import (
 
 	"github.com/lucasleonardobs/go-grpc-server/pb"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
-	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("Could not connect to gRPC server: %v", err)
 	}
